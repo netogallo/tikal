7ed1e7311e2f1a87659e0a3bb71c21a6c2273ee8
netogallo <connect@netowork.me>
2024-09-27 11:42:17 +0200

    Allow overriding the context.

--- base/Tikal/Test.nix
+++ base/Tikal/Test.nix
@@ -1,5 +1,11 @@
 {
-  __functor = _: { ... }: {
-    value = 42;
-  };
+  __functor = _: { context, ... }:
+    let
+      test-ctx = context {
+        name = "test-ctx";
+        members = {};
+      };
+    in
+      { value = test-ctx 42; }
+  ;
 }
--- bootstrap/context.nix
+++ bootstrap/context.nix
@@ -1,5 +1,7 @@
 { nixpkgs, tikal-meta }: { module-meta, test }:
 let
+  lib = nixpkgs.lib;
+  base-ctx-uid = "${tikal-meta.context-uid}-base";
 
   member-defaults = { fullname }: {
     __override = _: throw "The member '${fullname}' does not allow overriding.";
@@ -31,17 +33,29 @@ let
 
       apply-overrides = initial: final:
         let
-          override-member = current-member: new-member:
+          initial-ctx = initial.${tikal-meta.context-uid};
+          get-member = key: members-uid:
+            let
+              members = initial-ctx.${members-uid}.members;
+            in
+              if builtins.hasAttr key members
+              then [ members.${key} ]
+              else []
+          ;
+          initial-members = key: builtins.concatMap (get-member key) initial-ctx.contexts;
+          override-member-acc = current-member: new-member:
             {
-              __functor = self: current-member.__override { member = new-member; };
+              __functor = self: current-member.__override { super-ctx = initial; member = new-member; };
               __override = override-args: current-member.__override {
+                super-ctx = final;
                 member = (new-member.__override override-args);
               };
             }
           ;
+          override-member = { key, member }: lib.foldr override-member-acc member (initial-members key);
           apply-override = key: member:
             if key != tikal-meta.context-uid && builtins.hasAttr key initial
-            then override-member initial.${key} member final
+            then override-member { inherit key member; } final
             else member final
           ;
         in
@@ -51,13 +65,25 @@ let
         let
           base-ctx = {
             ${tikal-meta.context-uid} = {
-              contexts = [ self.uid ];
+              contexts = [ base-ctx-uid self.uid ];
               ${self.uid} = self;
+              ${base-ctx-uid} = {
+                name = "base";
+                members = {
+                  prim = member-defaults { fullname = "${self.fullname}.prim"; };
+                  extend = member-defaults { fullname = "${self.fullname}.extend"; };
+                };
+              };
             };
 
-            prim = (member-defaults { fullname = "${self.fullname}.prim"; } // {
-              __functor = _: self: value; 
-            }) ctx;
+            prim = value;
+
+            extend = new-ctx:
+              let
+                result = ctx // new-ctx.apply-overrides ctx result;
+              in
+                result
+            ;
           };
           ctx = base-ctx // apply-overrides base-ctx ctx;
         in
@@ -72,6 +98,44 @@ let
           expected = 42;
         in
           _assert ((ctx expected).prim == expected)
+      ;
+      "It can declare new members" = { _assert, ... }:
+        let
+          create-context = context {
+            name = "declare-member-test";
+            members = {
+              test = {
+                __functor = _: ctx: ctx.prim + 1;
+              };
+            };
+          };
+          expected = 42;
+        in
+          _assert ((create-context 41).test == expected)
+      ;
+      "It can override members" = { _assert, ... }:
+        let
+          ctx-1 = context {
+            name = "override-ctx1";
+            members = {
+              test = {
+                __functor = _: ctx: ctx.prim + 1;
+                __override = { super-ctx, member, ... }: ctx: super-ctx.test + member ctx;
+              };
+            };
+          };
+          ctx-2 = context {
+            name = "override-ctx2";
+            members = {
+              test = {
+                __functor = _: ctx: ctx.prim * 2;
+              };
+            };
+          };
+          value = (ctx-1 42).extend ctx-2;
+          expected = 84 + 43;
+        in
+          _assert (value.test == expected)
       ;
     };
   };
